ea7bce645e3d878ee9fbe775d625ff3d0932de58
Rohit <rohitbulusu@gmail.com>
2020-10-18 19:34:38 -0400

    added dynamic search bar

--- src/App.js
+++ src/App.js
@@ -2,40 +2,50 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+  const [searchTerm, setSearchTerm] = React.useState("");
 
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-    <hr />
-    <List />
-  </div>
-);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-const List = () =>
-  list.map(item => (
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <div key={list.objectID}>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
+
+const List = (props) =>
+  props.list.map((item) => (
+    <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
       </span>
@@ -43,7 +53,6 @@ const List = () =>
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
     </div>
-
-));
+  ));
 
 export default App;
